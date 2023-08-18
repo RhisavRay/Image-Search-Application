2d83dd082ada7f3675b567475563de1363e83e5d
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-18 08:23:26 +0530

    Variables for elements initialized

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const accessKey = "wevqFwD9dNHbB2snHcHmVjglOcBBh-cqOH80oI0XUIw"
 
+const formEl = document.querySelector(form)
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
