56a6efb511f911e954717b6aa1c1fc749777663c
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-18 09:47:39 +0530

    Issues fixed

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function searchImages()
 {
     inputData = inputEl.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
-    
+
     const response = await fetch(url)
     const data = await response.json()
     const results = data.results;
