4f404748908d156505e7cec74a61963457357eff
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-18 08:24:20 +0530

    variable to store input data and page count made

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const formEl = document.querySelector(form)
 const inputEl = document.getElementById("search-input")
 const searchResults = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
+
+let inputData = "";
+let page = 1;
