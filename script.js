a794c2feea408ae016e3e67b4c39c2a0af7fef70
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-18 08:29:39 +0530

    URL created and fetched

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const showMore = document.getElementById("show-more-button")
 
 let inputData = "";
 let page = 1;
+
+async function searchImages()
+{
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    
+    const response = await fetch(url)
+    const data = await response.json()
+
+    
+}
