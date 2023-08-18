168868def16a7bbb60536566d87f983311d03b17
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2023-08-18 08:40:52 +0530

    a tag added, href, target and text added

--- script.js
+++ script.js
@@ -26,6 +26,9 @@ async function searchImages()
         const image = document.createElement('img')
         image.src = result.urls.small
         image.alt = result.alt_description
-        
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = "_blank"
+        imageLink.textContent = result.alt_description
     })
 }
